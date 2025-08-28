f2eb7125e039f9aa09206b2b5b3c04c03c6bc37c
Kergalli <151893421+Kergalli@users.noreply.github.com>
2025-08-28 11:35:31 -0500

    Update main.js to v.1.3.9

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ import { DragonbaneUtils } from './utils.js';
 
 class DragonbaneActionRules {
     static ID = 'dragonbane-action-rules';
-    static VERSION = '1.3.8';
+    static VERSION = '1.3.9';
     
     static FLAGS = {
         RULES_MESSAGE: 'dragonbaneRulesMessage'
